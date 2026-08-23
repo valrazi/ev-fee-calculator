fe6bac44dde004c69893958f44a00757f07aafdd
Ivallavi Fahrazi <ivallavi.fahrazi@dansmultipro.com>
2026-08-23 14:50:30 +0700

    chore: initialize Spring Boot project with Java 17

---
+++ src/main/java/com/ivallavi/evmoto/fee_calculator/FeeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.ivallavi.evmoto.fee_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeeCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeeCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivallavi/evmoto/fee_calculator/FeeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivallavi.evmoto.fee_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeeCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
